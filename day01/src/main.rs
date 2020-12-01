5d8b8e1c35ed52613d83452a54b736dca7a5cfe3
Ruud van der Weide <rj@rdcl.dev>
2020-12-01 12:24:02 +0100

    Day 1, part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,10 +10,22 @@ fn main() {
         values.push(line);
     }
 
+    println!("Part 1:");
     for (i, v1) in values.iter().enumerate() {
         for v2 in values.iter().skip(i + 1) {
             if v1 + v2 == 2020 {
-                println!("{} + {} = 2020, {} * {} = {}", v1, v2, v1, v2, v1 * v2);
+                println!("  {} + {} = 2020, {} * {} = {}", v1, v2, v1, v2, v1 * v2);
+            }
+        }
+    }
+
+    println!("Part 2:");
+    for (i, v1) in values.iter().enumerate() {
+        for (j, v2) in values.iter().skip(i + 1).enumerate() {
+            for v3 in values.iter().skip(j + 1) {
+                if v1 + v2 + v3 == 2020 {
+                    println!("  {} + {} + {} = 2020, {} * {} * {} = {}", v1, v2, v3, v1, v2, v3, v1 * v2 * v3);
+                }
             }
         }
     }
