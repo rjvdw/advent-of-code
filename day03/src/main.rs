eb6f4eb2eb7be0520f7aadd5ef60c95198f55569
Ruud van der Weide <rj@rdcl.dev>
2020-12-03 19:08:49 +0100

    Modified solution for day 3 to accept command line parameters

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -11,22 +11,29 @@ use input_record::InputRecord;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        eprintln!("Usage: {} <input file>", &args[0]);
+    if args.len() < 3 {
+        eprintln!("Usage: {} <input file> <x1>,<y1> ... <xn>,<yn>", &args[0]);
         exit(1);
     }
 
     let path = &args[1];
     let values: Vec<InputRecord> = handle_result(read_input(path));
 
-    println!("{}", solve(&values, 3, 1));
+    let mut result = 1;
+    for entry in args.iter().skip(2) {
+        let vec = entry.split(',')
+            .map(|c| handle_result(c.parse::<usize>()))
+            .collect::<Vec<usize>>();
 
-    println!("{}", solve(&values, 1, 1)
-        * solve(&values, 3, 1)
-        * solve(&values, 5, 1)
-        * solve(&values, 7, 1)
-        * solve(&values, 1, 2)
-    );
+        if let [x, y] = &vec[..] {
+            result *= solve(&values, *x, *y);
+        } else {
+            eprintln!("Invalid input: {}", entry);
+            exit(1);
+        }
+    }
+
+    println!("{}", result);
 }
 
 fn solve(values: &Vec<InputRecord>, step_x: usize, step_y: usize) -> u64 {
