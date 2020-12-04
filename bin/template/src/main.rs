44421b899c179d3f0961bb83cc83297f553d0851
Ruud van der Weide <rj@rdcl.dev>
2020-12-04 17:05:02 +0100

    Added script to generate cargo project using a template

---
+++ bin/template/src/main.rs
@@ -0,0 +1,16 @@
+extern crate helpers;
+
+/// https://adventofcode.com/2020/day/#
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        //
+    }
+}
