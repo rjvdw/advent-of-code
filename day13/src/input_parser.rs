3a94ec95cae82ef7e65420b912ccc3bd9dd43a7f
Ruud van der Weide <rj@rdcl.dev>
2020-12-13 11:13:17 +0100

    Added solution to day 13

---
+++ day13/src/input_parser.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use helpers::ParseError;
+
+pub fn read(path: &str) -> Result<(u32, Vec<u32>), ParseError> {
+    let file = File::open(path)?;
+    let mut lines = BufReader::new(file).lines();
+    let earliest_departure = match lines.next() {
+        Some(Ok(line)) => line.parse::<u32>()?,
+        _ => return Err(ParseError("Input file has insufficient lines".to_string())),
+    };
+    let mut schedule = Vec::new();
+    match lines.next() {
+        Some(Ok(line)) => {
+            for line in line.split(',') {
+                if line == "x" {
+                    schedule.push(0);
+                } else {
+                    schedule.push(line.parse::<u32>()?);
+                }
+            }
+        }
+        _ => return Err(ParseError("Input file has insufficient lines".to_string())),
+    };
+
+    Ok((earliest_departure, schedule))
+}
---
+++ day13/src/main.rs
@@ -0,0 +1,123 @@
+extern crate helpers;
+
+use std::env;
+use std::process::exit;
+
+use helpers::handle_result;
+
+use crate::math::solve_crt;
+
+mod input_parser;
+mod math;
+
+/// https://adventofcode.com/2020/day/13
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <input file>", &args[0]);
+        exit(1);
+    }
+
+    let (earliest_departure, schedule) = handle_result(input_parser::read(&args[1]));
+
+    match find_shortest_wait_time(earliest_departure, &schedule) {
+        Some((id, wait_time)) => println!(
+            "Solution for part 1: {} x {} = {}",
+            id,
+            wait_time,
+            id * wait_time,
+        ),
+        None => println!("Could not find a solution for part 1"),
+    }
+
+    println!("The answer to the contest is: {}", win_contest(&schedule));
+}
+
+fn find_shortest_wait_time(earliest_departure: u32, schedule: &[u32]) -> Option<(u32, u32)> {
+    schedule
+        .iter()
+        .filter(|&&v| v != 0)
+        .map(|&v| (v, v - (earliest_departure % v)))
+        .min_by_key(|&v| v.1)
+}
+
+/// Find the solution to the contest. We assume all bus IDs are pairwise coprime.
+fn win_contest(schedule: &[u32]) -> u64 {
+    let input = schedule
+        .iter()
+        .enumerate()
+        .filter(|&(_, &a)| a != 0)
+        .map(|(a, &n)| (n as u64, a as u64)) // swap the inputs and cast to i64
+        .collect::<Vec<(u64, u64)>>();
+
+    match input.first() {
+        Some(&first) => {
+            let (p, n) = input
+                .iter()
+                .skip(1)
+                .fold(first, |p1, &p2| (p1.0 * p2.0, solve_crt(p1, p2)));
+
+            p - n
+        }
+        None => 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let earliest_departure = 939;
+        let schedule = vec![7, 13, 0, 0, 59, 0, 31, 19];
+
+        assert_eq!(
+            find_shortest_wait_time(earliest_departure, &schedule),
+            Some((59, 5))
+        );
+    }
+
+    #[test]
+    fn test_part_2_1() {
+        let schedule = vec![7, 13, 0, 0, 59, 0, 31, 19];
+
+        assert_eq!(win_contest(&schedule), 1068781);
+    }
+
+    #[test]
+    fn test_part_2_2() {
+        let schedule = vec![17, 0, 13, 19];
+
+        assert_eq!(win_contest(&schedule), 3417);
+    }
+
+    #[test]
+    fn test_part_2_3() {
+        let schedule = vec![67, 7, 59, 61];
+
+        assert_eq!(win_contest(&schedule), 754018);
+    }
+
+    #[test]
+    fn test_part_2_4() {
+        let schedule = vec![67, 0, 7, 59, 61];
+
+        assert_eq!(win_contest(&schedule), 779210);
+    }
+
+    #[test]
+    fn test_part_2_5() {
+        let schedule = vec![67, 7, 0, 59, 61];
+
+        assert_eq!(win_contest(&schedule), 1261476);
+    }
+
+    #[test]
+    fn test_part_2_6() {
+        let schedule = vec![1789, 37, 47, 1889];
+
+        assert_eq!(win_contest(&schedule), 1202161486);
+    }
+}
