2d1b826d11bcac7ab89f785ab49cd593443b7706
Ruud van der Weide <rj@rdcl.dev>
2020-12-26 20:23:19 +0100

    Added solution for 2015, day 4

--- 2015/README.md
+++ 2015/README.md
@@ -4,6 +4,10 @@
 
 My answers for the 2015 [Advent of Code](https://adventofcode.com/2015).
 
+## To Do
+
+* For day 4, it might be smarter to compute the MD5 hash myself. Need to investigate...
+
 
 [workflows-CI-badge]: https://github.com/rjvdw/advent-of-code/workflows/CI%202015/badge.svg
 [actions]: https://github.com/rjvdw/advent-of-code/actions?query=workflow%3A%22CI+2015%22
---
+++ 2015/day04/src/main.rs
@@ -0,0 +1,39 @@
+use rdcl_aoc_helpers::args::get_args;
+
+fn main() {
+    let args = get_args(&["<secret key>"], 1);
+
+    let (hash, advent_coin) = mine(&args[1], "00000");
+    println!("For number {}, the hash is {}", advent_coin, hash);
+
+    let (hash, advent_coin) = mine(&args[1], "000000");
+    println!("For number {}, the hash is {}", advent_coin, hash);
+}
+
+fn mine(secret_key: &str, lead: &str) -> (String, usize) {
+    let mut number = 0;
+    loop {
+        number += 1;
+        let input = format!("{}{}", secret_key, number);
+        let hash = md5::compute(input);
+        let formatted = format!("{:x}", hash);
+        if formatted.starts_with(lead) {
+            return (formatted, number);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mine_1() {
+        assert_eq!(mine("abcdef", "00000").1, 609043);
+    }
+
+    #[test]
+    fn test_mine_2() {
+        assert_eq!(mine("pqrstuv", "00000").1, 1048970);
+    }
+}
