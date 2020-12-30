8117770e44e13d4345e1b63715998cf61bebb437
Ruud van der Weide <rj@rdcl.dev>
2020-12-30 15:52:37 +0100

    Added solution for 2016, day 4

---
+++ 2016/day04/src/main.rs
@@ -0,0 +1,70 @@
+use std::fs::File;
+
+use rdcl_aoc_helpers::args::get_args;
+use rdcl_aoc_helpers::input::WithReadLines;
+
+use crate::room::Room;
+
+mod room;
+
+fn main() {
+    let args = get_args(&["<Input file>"], 1);
+    let rooms = File::open(&args[1]).read_lines::<Room>(1);
+
+    let valid_rooms: Vec<Room> = rooms.filter(|room| room.is_valid()).collect();
+
+    println!(
+        "The sum of the sectors of all the valid rooms is {}.",
+        valid_rooms
+            .iter()
+            .map(|room| room.get_sector())
+            .sum::<u32>()
+    );
+
+    for room in valid_rooms {
+        let decrypted = room.decrypt();
+
+        if decrypted.contains("north") && decrypted.contains("pole") {
+            println!(
+                "Room \"{}\" could be it (sector = {})",
+                decrypted,
+                room.get_sector()
+            );
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid_1() {
+        let room = "aaaaa-bbb-z-y-x-123[abxyz]".parse::<Room>().unwrap();
+        assert!(room.is_valid());
+    }
+
+    #[test]
+    fn test_is_valid_2() {
+        let room = "a-b-c-d-e-f-g-h-987[abcde]".parse::<Room>().unwrap();
+        assert!(room.is_valid());
+    }
+
+    #[test]
+    fn test_is_valid_3() {
+        let room = "not-a-real-room-404[oarel]".parse::<Room>().unwrap();
+        assert!(room.is_valid());
+    }
+
+    #[test]
+    fn test_is_valid_4() {
+        let room = "totally-real-room-200[decoy]".parse::<Room>().unwrap();
+        assert!(!room.is_valid());
+    }
+
+    #[test]
+    fn test_decrypt() {
+        let room = "qzmt-zixmtkozy-ivhz-343[abcde]".parse::<Room>().unwrap();
+        assert_eq!(room.decrypt(), "very encrypted name".to_string());
+    }
+}
---
+++ 2016/day04/src/room.rs
@@ -0,0 +1,90 @@
+use std::convert::TryFrom;
+use std::str::FromStr;
+
+use rdcl_aoc_helpers::error::ParseError;
+
+const LOWER: u8 = b'a';
+const MODULUS: u8 = 26;
+
+#[derive(Debug)]
+pub struct Room {
+    name: String,
+    sector: u32,
+    checksum: String,
+}
+
+impl Room {
+    pub fn get_sector(&self) -> u32 {
+        self.sector
+    }
+
+    pub fn is_valid(&self) -> bool {
+        let mut frequencies = [0u8; MODULUS as usize];
+
+        for ch in self.name.chars() {
+            if ch != '-' {
+                let idx = ((ch as u8) - LOWER) as usize;
+                frequencies[idx] += 1;
+            }
+        }
+
+        let mut max = u8::MAX;
+        let mut checksum = String::new();
+        while checksum.len() < self.checksum.len() {
+            let mut local_max = u8::MIN;
+            for &freq in &frequencies {
+                if freq < max && freq > local_max {
+                    local_max = freq;
+                }
+            }
+            for (idx, _) in frequencies
+                .iter()
+                .enumerate()
+                .filter(|(_, &freq)| freq == local_max)
+            {
+                let ch = (u8::try_from(idx).unwrap() + LOWER) as char;
+                checksum.push(ch);
+                if checksum.len() == self.checksum.len() {
+                    break;
+                }
+            }
+            max = local_max;
+        }
+
+        checksum == self.checksum
+    }
+
+    pub fn decrypt(&self) -> String {
+        let mut decrypted = String::new();
+        let shift = u8::try_from(self.sector % (MODULUS as u32)).unwrap();
+        for ch in self.name.chars() {
+            if ch == '-' {
+                decrypted.push(' ');
+            } else {
+                let ch = ((((ch as u8) - LOWER + shift) % MODULUS) + LOWER) as char;
+                decrypted.push(ch);
+            }
+        }
+        decrypted
+    }
+}
+
+impl FromStr for Room {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let (Some(idx1), Some(idx2)) = (s.rfind('-'), s.rfind('[')) {
+            let name = s[..idx1].to_string();
+            let sector = s[idx1 + 1..idx2].parse()?;
+            let checksum = s[idx2 + 1..s.len() - 1].to_string();
+
+            Ok(Room {
+                name,
+                sector,
+                checksum,
+            })
+        } else {
+            Err(ParseError(format!("Invalid input: {}", s)))
+        }
+    }
+}
