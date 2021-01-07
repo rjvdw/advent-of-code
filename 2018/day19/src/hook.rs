ab2ff0417a83419cd464faad4932678c400b0966
Ruud van der Weide <rj@rdcl.dev>
2021-01-07 12:14:55 +0100

    Removed unneeded optimizations, so that solution becomes more generic.

--- 2018/day19/src/hook.rs
+++ 2018/day19/src/hook.rs
@@ -21,35 +21,19 @@ impl PreExecuteHook<Instruction> for Hook {
         instruction: &Instruction,
         idx: usize,
     ) -> HookResult {
-        if self.1 {
-            // todo: These optimizations are based on my input. YMMV.
-            if idx == 1 {
-                let f = machine.register.read('f');
-                machine.register.write('a', find_sum_of_divisors(f));
-                machine.register.write('c', 324);
-            } else if idx == 17 {
-                let a = machine.register.read('a');
-                let d = machine.register.read('d');
-                let f = machine.register.read('f');
+        if self.1 && idx == 1 {
+            let f = machine.register.read('f');
 
-                let new_d = 22 * d + 181;
-                let new_f = 209 * (f + 2) * (f + 2) + new_d;
-
-                machine.register.write('c', 25 + a);
-                machine.register.write('d', new_d);
-                machine.register.write('f', new_f);
-            } else if idx == 27 {
-                machine.register.write('a', 0);
-                machine.register.write('c', 0);
-                machine.register.write('d', 10550400);
-                machine.register.increment('f', 10550400);
-            } else {
-                instruction.execute(&mut machine.register, &mut machine.output_receiver);
-            }
+            // Strictly speaking, we don't need to set all these registers (just 'a' and 'c'), but I
+            // think it's nice if the entire machine halts in the correct state.
+            machine.register.write('a', find_sum_of_divisors(f));
+            machine.register.write('b', f);
+            machine.register.write('c', 15);
+            machine.register.write('d', 1);
+            machine.register.write('e', f + 1);
         } else {
             instruction.execute(&mut machine.register, &mut machine.output_receiver);
         }
-
         machine.register.increment(self.0, 1);
         HookResult::Goto(machine.register.read(self.0))
     }
