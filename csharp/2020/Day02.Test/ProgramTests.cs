a71295e22de0d16b4daf1333fd1a73bc09294a7e
Ruud van der Weide <rj@rdcl.dev>
2021-11-18 08:33:43 +0100

    Added a C# solution for 2020, day 2

---
+++ csharp/2020/Day02.Test/ProgramTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace Day02.Test;
+
+public class ProgramTests
+{
+    [Fact]
+    public void TestIsValidPassword1()
+    {
+        Assert.True(PasswordEntry.Parse("1-3 a: abcde").IsValidAccordingTo1());
+        Assert.False(PasswordEntry.Parse("1-3 b: cdefg").IsValidAccordingTo1());
+        Assert.True(PasswordEntry.Parse("2-9 c: ccccccccc").IsValidAccordingTo1());
+    }
+
+    [Fact]
+    public void TestIsValidPassword2()
+    {
+        Assert.True(PasswordEntry.Parse("1-3 a: abcde").IsValidAccordingTo2());
+        Assert.False(PasswordEntry.Parse("1-3 b: cdefg").IsValidAccordingTo2());
+        Assert.False(PasswordEntry.Parse("2-9 c: ccccccccc").IsValidAccordingTo2());
+    }
+}
---
+++ csharp/2020/Day02/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Day02
+{
+    // https://adventofcode.com/2020/day/2
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: $0 <input file>");
+                Environment.Exit(1);
+                return;
+            }
+
+            var inputFile = args[0];
+            var lines = File
+                .ReadAllLines(inputFile)
+                .ToList();
+
+            var nrValidPasswords1 = lines
+                .Select(PasswordEntry.Parse)
+                .Count(entry => entry.IsValidAccordingTo1());
+
+            Console.WriteLine($"The solution to part 1 is: {nrValidPasswords1}");
+
+            var nrValidPasswords2 = lines
+                .Select(PasswordEntry.Parse)
+                .Count(entry => entry.IsValidAccordingTo2());
+
+            Console.WriteLine($"The solution to part 2 is: {nrValidPasswords2}");
+        }
+    }
+
+    public record PasswordEntry(
+        int Lower,
+        int Upper,
+        char Character,
+        string Password)
+    {
+        public bool IsValidAccordingTo1()
+        {
+            var count = Password
+                .ToCharArray()
+                .Count(ch => ch == Character);
+
+            return count >= Lower && count <= Upper;
+        }
+
+        public bool IsValidAccordingTo2()
+        {
+            var l = Password[Lower - 1] == Character;
+            var h = Password[Upper - 1] == Character;
+            return (l || h) && !(l && h);
+        }
+
+        public static PasswordEntry Parse(string line)
+        {
+            var parts = line.Split(": ");
+            var password = parts[1];
+            parts = parts[0].Split(" ");
+            var character = char.Parse(parts[1]);
+            parts = parts[0].Split("-");
+            var lower = int.Parse(parts[0]);
+            var upper = int.Parse(parts[1]);
+
+            return new PasswordEntry(lower, upper, character, password);
+        }
+    }
+}
