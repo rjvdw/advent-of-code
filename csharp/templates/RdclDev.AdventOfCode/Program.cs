6178521b04bccf765c0eb5c2c5083c735f7a5792
Ruud van der Weide <rj@rdcl.dev>
2021-12-05 19:18:07 +0100

    Created template for new C# solutions

---
+++ csharp/templates/RdclDev.AdventOfCode/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace RdclDev.AdventOfCode;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ csharp/templates/RdclDev.AdventOfCode/ProgramTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace RdclDev.AdventOfCode;
+
+public class ProgramTest
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(0, 0);
+    }
+}
