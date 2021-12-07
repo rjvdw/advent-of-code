b84adbe7dc613a14dc38e5ae34d08d9737d45496
Ruud van der Weide <rj@rdcl.dev>
2021-12-07 07:03:24 +0100

    Use a global cache for day 6

--- rust/2021/day06/src/main.rs
+++ rust/2021/day06/src/main.rs
@@ -1,7 +1,10 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate rdcl_aoc_helpers;
 
 use std::collections::HashMap;
 use std::fs::File;
+use std::sync::Mutex;
 
 use rdcl_aoc_helpers::args::get_args;
 use rdcl_aoc_helpers::error::WithOrExit;
@@ -9,6 +12,10 @@ use rdcl_aoc_helpers::input::WithReadLines;
 
 const GESTATION_PERIOD: usize = 9;
 const REPRODUCTION_RATE: usize = 7;
+
+lazy_static! {
+    static ref CACHE: Mutex<HashMap<usize, usize>> = Mutex::new(HashMap::new());
+}
 
 /// https://adventofcode.com/2021/day/6
 fn main() {
@@ -31,26 +38,24 @@ fn main() {
 }
 
 fn solve(values: &[usize], n: usize) -> usize {
-    let mut cache = HashMap::new();
-
     values
         .iter()
-        .map(|v| fibo_alt(n + GESTATION_PERIOD - v - 1, &mut cache))
+        .map(|v| fibo_alt(n + GESTATION_PERIOD - v - 1))
         .sum()
 }
 
-fn fibo_alt(n: usize, cache: &mut HashMap<usize, usize>) -> usize {
-    match cache.get(&n) {
-        Some(v) => *v,
-        None => {
-            let v = if n < GESTATION_PERIOD {
-                1
-            } else {
-                fibo_alt(n - GESTATION_PERIOD, cache) + fibo_alt(n - REPRODUCTION_RATE, cache)
-            };
-            cache.insert(n, v);
-            v
-        }
+fn fibo_alt(n: usize) -> usize {
+    let opt = CACHE.lock().unwrap().get(&n).copied();
+    if let Some(v) = opt {
+        v
+    } else {
+        let v = if n < GESTATION_PERIOD {
+            1
+        } else {
+            fibo_alt(n - GESTATION_PERIOD) + fibo_alt(n - REPRODUCTION_RATE)
+        };
+        CACHE.lock().unwrap().insert(n, v);
+        v
     }
 }
 
