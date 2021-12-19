fe61dea0380b687c917fb19e0127b8b331b7f29e
Ruud van der Weide <rj@rdcl.dev>
2021-12-19 12:53:08 +0100

    Implemented Point class

---
+++ csharp/AoC2021/Day19/Day19/Program.cs
@@ -0,0 +1,13 @@
+﻿// https://adventofcode.com/.......
+
+using Day19;
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine("Usage: $0 <input file>");
+    Environment.Exit(1);
+}
+
+var lines = File.ReadLines(args[0]);
+
+Console.WriteLine(Solution.Solve(lines));
---
+++ csharp/AoC2021/Day19/Day19/Solution.cs
@@ -0,0 +1,9 @@
+namespace Day19;
+
+public static class Solution
+{
+    public static string Solve(IEnumerable<string> lines)
+    {
+        return $"Hello, World! [{string.Join(", ", lines)}]";
+    }
+}
