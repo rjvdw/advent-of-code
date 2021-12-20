dd902f16ce68ea5e2c397fefcc7aec227ba424da
Ruud van der Weide <rj@rdcl.dev>
2021-12-20 17:58:34 +0100

    Added unit test for Image

--- csharp/AoC2021/Day20/Day20/Bounds.cs
+++ csharp/AoC2021/Day20/Day20/Bounds.cs
@@ -2,33 +2,36 @@ namespace Day20;
 
 public class Bounds
 {
-    public (long Row, long Col) TopLeft;
-    public (long Row, long Col) BottomRight;
+    private (long Row, long Col) _topLeft;
+    private (long Row, long Col) _bottomRight;
+
+    public (long Row, long Col) TopLeft => _topLeft;
+    public (long Row, long Col) BottomRight => _bottomRight;
 
     public Bounds((long Row, long Col) topLeft, (long Row, long Col) bottomRight)
     {
-        TopLeft = topLeft;
-        BottomRight = bottomRight;
+        _topLeft = topLeft;
+        _bottomRight = bottomRight;
     }
 
     public void UpdateWith(long row, long col)
     {
-        if (row < TopLeft.Row) TopLeft.Row = row;
-        if (row > BottomRight.Row) BottomRight.Row = row;
-        if (col < TopLeft.Col) TopLeft.Col = col;
-        if (col > BottomRight.Col) BottomRight.Col = col;
+        if (row < _topLeft.Row) _topLeft.Row = row;
+        if (row > _bottomRight.Row) _bottomRight.Row = row;
+        if (col < _topLeft.Col) _topLeft.Col = col;
+        if (col > _bottomRight.Col) _bottomRight.Col = col;
     }
 
     public Bounds Stretched(long by) => new(
-        (TopLeft.Row - by, TopLeft.Col - by),
-        (BottomRight.Row + by, BottomRight.Col + by)
+        (_topLeft.Row - by, _topLeft.Col - by),
+        (_bottomRight.Row + by, _bottomRight.Col + by)
     );
 
     public IEnumerable<(long Row, long Col)> IterRowCol()
     {
-        for (var row = TopLeft.Row; row <= BottomRight.Row; row += 1)
+        for (var row = _topLeft.Row; row <= _bottomRight.Row; row += 1)
         {
-            for (var col = TopLeft.Col; col <= BottomRight.Col; col += 1)
+            for (var col = _topLeft.Col; col <= _bottomRight.Col; col += 1)
             {
                 yield return (row, col);
             }
@@ -36,6 +39,6 @@ public class Bounds
     }
 
     public bool Contains(long row, long col) =>
-        row >= TopLeft.Row && row <= BottomRight.Row &&
-        col >= TopLeft.Col && col <= BottomRight.Col;
+        row >= _topLeft.Row && row <= _bottomRight.Row &&
+        col >= _topLeft.Col && col <= _bottomRight.Col;
 }
