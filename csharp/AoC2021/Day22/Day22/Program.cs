acd352769ddf8963d8ffc4a3993aedcf43e57f59
Ruud van der Weide <rj@rdcl.dev>
2021-12-22 11:41:29 +0100

    Ported the rest of the program

--- csharp/AoC2021/Day22/Day22/Program.cs
+++ csharp/AoC2021/Day22/Day22/Program.cs
@@ -8,7 +8,7 @@ if (args.Length != 1)
 
 var cubes = File.ReadLines(args[0]).Select(Cuboid.Parse);
 
-var (init, full) = Solution.Solve(cubes);
+var (init, full) = Solution.ExecuteRebootSequence(cubes);
 
 Console.WriteLine($"After the initialization sequence, {init} cubes are on.");
 Console.WriteLine($"After the full reboot sequence, {full} cubes are on.");
--- csharp/AoC2021/Day22/Day22/Solution.cs
+++ csharp/AoC2021/Day22/Day22/Solution.cs
@@ -2,10 +2,49 @@ namespace Day22;
 
 public static class Solution
 {
-    public static (long InitializationSequenceCount, long TotalCount) Solve(IEnumerable<Cuboid> cuboids)
-    {
-        Console.WriteLine($"Hello, World! [{string.Join(", ", cuboids)}]");
+    private static readonly Cuboid InitializationRegion = new(
+        false, // irrelevant
+        new Range(-50, 50),
+        new Range(-50, 50),
+        new Range(-50, 50)
+    );
 
-        return (0, 0);
+    public static (long InitializationSequenceCount, long TotalCount) ExecuteRebootSequence(IEnumerable<Cuboid> cuboids)
+    {
+        var initDone = false;
+        var initCount = 0L;
+        var onRegions = new List<Cuboid>();
+
+        foreach (var cuboid in cuboids)
+        {
+            if (!initDone && !cuboid.FitsWithin(InitializationRegion))
+            {
+                initDone = true;
+                initCount = CountCubes(onRegions);
+            }
+
+            var nextOnRegions = new List<Cuboid>();
+
+            foreach (var region in onRegions)
+            {
+                var subRegions = region.Subtract(cuboid);
+                if (subRegions is null)
+                    nextOnRegions.Add(region);
+                else
+                    nextOnRegions.AddRange(subRegions);
+            }
+
+            if (cuboid.IsOn)
+                nextOnRegions.Add(cuboid);
+
+            onRegions = nextOnRegions;
+        }
+
+        if (!initDone)
+            initCount = CountCubes(onRegions);
+
+        return (initCount, CountCubes(onRegions));
     }
+
+    private static long CountCubes(IEnumerable<Cuboid> cuboids) => cuboids.Select(cuboid => cuboid.Size()).Sum();
 }
