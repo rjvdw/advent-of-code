758f3b78b50e6e2b09e7743dbdac208a53f32326
Ruud van der Weide <rj@rdcl.dev>
2021-12-22 10:11:11 +0100

    Added a rust solution for 2021, day 22

---
+++ rust/2021/day22/src/cuboid.rs
@@ -0,0 +1,178 @@
+use std::fmt;
+use std::str::FromStr;
+
+use rdcl_aoc_helpers::error::ParseError;
+use rdcl_aoc_helpers::parse_error;
+
+use crate::range::Range;
+
+#[derive(Copy, Clone)]
+pub struct Cuboid {
+    pub is_on: bool,
+    pub x_range: Range,
+    pub y_range: Range,
+    pub z_range: Range,
+}
+
+impl Cuboid {
+    /// If the cuboid has overlap with the other cuboid, return a selection of cuboids that together
+    /// contain all points that are in the first cuboid, but not in the second.
+    pub fn subtract(&self, other: &Cuboid) -> Option<Vec<Cuboid>> {
+        let mut subs = vec![];
+
+        let partition_x = self.x_range.partition(&other.x_range)?;
+        let partition_y = self.y_range.partition(&other.y_range)?;
+        let partition_z = self.z_range.partition(&other.z_range)?;
+
+        for x_range in &partition_x {
+            for y_range in &partition_y {
+                for z_range in &partition_z {
+                    let sub = Cuboid {
+                        is_on: self.is_on,
+                        x_range: *x_range,
+                        y_range: *y_range,
+                        z_range: *z_range,
+                    };
+
+                    if !sub.fits_within(other) {
+                        subs.push(sub);
+                    }
+                }
+            }
+        }
+
+        Some(subs)
+    }
+
+    pub fn fits_within(&self, other: &Cuboid) -> bool {
+        self.x_range.fits_within(&other.x_range)
+            && self.y_range.fits_within(&other.y_range)
+            && self.z_range.fits_within(&other.z_range)
+    }
+
+    pub fn size(&self) -> usize {
+        self.x_range.size() * self.y_range.size() * self.z_range.size()
+    }
+}
+
+impl fmt::Debug for Cuboid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} x={:?},y={:?},z={:?}",
+            if self.is_on { "on" } else { "off" },
+            self.x_range,
+            self.y_range,
+            self.z_range,
+        )
+    }
+}
+
+impl FromStr for Cuboid {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let i = if let Some(v) = find_indices(s) {
+            v
+        } else {
+            return Err(parse_error!("Invalid input string: {}", s));
+        };
+
+        Ok(Cuboid {
+            is_on: s.starts_with("on"),
+            x_range: s[i[0]..i[1]].parse()?,
+            y_range: s[i[2]..i[3]].parse()?,
+            z_range: s[i[4]..].parse()?,
+        })
+    }
+}
+
+fn find_indices(s: &str) -> Option<[usize; 5]> {
+    let mut i = [0; 5];
+
+    // x_range.from
+    i[0] = 2 + s.find("x=")?;
+
+    // x_range.to
+    i[1] = i[0] + s[i[0]..].find(",y=")?;
+
+    // y_range.from
+    i[2] = i[1] + 3;
+
+    // y_range.to
+    i[3] = i[2] + s[i[2]..].find(",z=")?;
+
+    // z_range.from
+    i[4] = i[3] + 3;
+
+    Some(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fits_within() {
+        let cuboid_a = Cuboid {
+            is_on: true,
+            x_range: Range::new(-5, 5),
+            y_range: Range::new(-5, 5),
+            z_range: Range::new(-5, 5),
+        };
+        let cuboid_b = Cuboid {
+            is_on: true,
+            x_range: Range::new(-10, 10),
+            y_range: Range::new(-10, 10),
+            z_range: Range::new(-10, 10),
+        };
+
+        assert!(cuboid_a.fits_within(&cuboid_a));
+        assert!(cuboid_a.fits_within(&cuboid_b));
+        assert!(cuboid_b.fits_within(&cuboid_b));
+        assert!(!cuboid_b.fits_within(&cuboid_a));
+    }
+
+    #[test]
+    fn test_size() {
+        let cuboid = Cuboid {
+            is_on: true,
+            x_range: Range::new(-5, 5),
+            y_range: Range::new(-5, 5),
+            z_range: Range::new(-5, 5),
+        };
+        assert_eq!(cuboid.size(), 11 * 11 * 11);
+    }
+
+    #[test]
+    fn test_from_str_1() {
+        let cuboid = "on x=1..2,y=3..4,z=5..6".parse::<Cuboid>().unwrap();
+
+        assert!(cuboid.is_on);
+        assert_eq!(cuboid.x_range.from, 1);
+        assert_eq!(cuboid.x_range.to, 2);
+        assert_eq!(cuboid.y_range.from, 3);
+        assert_eq!(cuboid.y_range.to, 4);
+        assert_eq!(cuboid.z_range.from, 5);
+        assert_eq!(cuboid.z_range.to, 6);
+    }
+
+    #[test]
+    fn test_from_str_2() {
+        let cuboid = "off x=1..2,y=3..4,z=5..6".parse::<Cuboid>().unwrap();
+
+        assert!(!cuboid.is_on);
+        assert_eq!(cuboid.x_range.from, 1);
+        assert_eq!(cuboid.x_range.to, 2);
+        assert_eq!(cuboid.y_range.from, 3);
+        assert_eq!(cuboid.y_range.to, 4);
+        assert_eq!(cuboid.z_range.from, 5);
+        assert_eq!(cuboid.z_range.to, 6);
+    }
+
+    #[test]
+    fn test_from_str_3() {
+        let result = "invalid".parse::<Cuboid>();
+        assert!(result.is_err());
+    }
+}
---
+++ rust/2021/day22/src/range.rs
@@ -0,0 +1,139 @@
+use std::fmt;
+use std::str::FromStr;
+
+use rdcl_aoc_helpers::error::ParseError;
+use rdcl_aoc_helpers::parse_error;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Range {
+    pub from: i64,
+    pub to: i64,
+}
+
+impl Range {
+    pub fn new(from: i64, to: i64) -> Range {
+        Range { from, to }
+    }
+
+    /// Creates a partition of `self` such that for every element in the partition the element is
+    /// either completely disjoint from `other`, or completely overlaps. If `self` and `other` are
+    /// completely disjoint to begin with, this method returns `None`.
+    pub fn partition(&self, other: &Range) -> Option<Vec<Range>> {
+        if self.to < other.from || self.from > other.to {
+            // completely disjoint
+            None
+        } else {
+            let mut partitions = vec![
+                // the part of `self` that overlaps with `other`
+                Range::new(
+                    [self.from, other.from].iter().max().copied().unwrap(),
+                    [self.to, other.to].iter().min().copied().unwrap(),
+                ),
+            ];
+
+            // the part of `self` that extends to the left of `other`
+            if self.from < other.from {
+                partitions.push(Range::new(self.from, other.from - 1));
+            }
+
+            // the part of `self` that extends to the right of `other`
+            if self.to > other.to {
+                partitions.push(Range::new(other.to + 1, self.to));
+            }
+
+            Some(partitions)
+        }
+    }
+
+    pub fn fits_within(&self, other: &Range) -> bool {
+        self.from >= other.from && self.to <= other.to
+    }
+
+    pub fn size(&self) -> usize {
+        (self.to - self.from) as usize + 1
+    }
+}
+
+impl fmt::Debug for Range {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}..{}", self.from, self.to)
+    }
+}
+
+impl FromStr for Range {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let i = if let Some(v) = s.find("..") {
+            v
+        } else {
+            return Err(parse_error!("Invalid range: {}", s));
+        };
+
+        let from = s[..i].parse()?;
+        let to = s[i + 2..].parse()?;
+        if from > to {
+            return Err(parse_error!("Invalid range: {}", s));
+        }
+
+        Ok(Range { from, to })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_partition() {
+        assert_eq!(
+            Range::new(0, 10).partition(&Range::new(5, 15)),
+            Some(vec![Range::new(5, 10), Range::new(0, 4)]),
+        );
+
+        assert_eq!(Range::new(0, 10).partition(&Range::new(15, 25)), None);
+
+        assert_eq!(
+            Range::new(0, 10).partition(&Range::new(2, 7)),
+            Some(vec![Range::new(2, 7), Range::new(0, 1), Range::new(8, 10)]),
+        );
+
+        assert_eq!(
+            Range::new(2, 7).partition(&Range::new(0, 10)),
+            Some(vec![Range::new(2, 7)]),
+        );
+    }
+
+    #[test]
+    fn test_fits_within() {
+        let range_a = Range::new(-5, 5);
+        let range_b = Range::new(-10, 10);
+        let range_c = Range::new(0, 10);
+
+        assert!(range_a.fits_within(&range_a));
+        assert!(range_a.fits_within(&range_b));
+        assert!(!range_a.fits_within(&range_c));
+        assert!(!range_b.fits_within(&range_a));
+        assert!(range_b.fits_within(&range_b));
+        assert!(!range_b.fits_within(&range_c));
+        assert!(!range_c.fits_within(&range_a));
+        assert!(range_c.fits_within(&range_b));
+        assert!(range_c.fits_within(&range_c));
+    }
+
+    #[test]
+    fn test_size() {
+        assert_eq!(Range::new(-7, 12).size(), 20);
+        assert_eq!(Range::new(7, 12).size(), 6);
+    }
+
+    #[test]
+    fn test_from_str() {
+        let range = "5..10".parse::<Range>().unwrap();
+        assert_eq!(range.from, 5);
+        assert_eq!(range.to, 10);
+
+        let range = "invalid".parse::<Range>();
+        assert!(range.is_err());
+    }
+}
