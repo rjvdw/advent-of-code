856b91430a7fc4625ac671e82ecb7e85330bc708
Ruud van der Weide <rj@rdcl.dev>
2021-12-25 08:55:07 +0100

    Implemented TryFindMoveToSideRoom and FindMovesToHallway

--- csharp/AoC2021/Day23/Day23/Burrow.cs
+++ csharp/AoC2021/Day23/Day23/Burrow.cs
@@ -2,6 +2,8 @@ namespace Day23;
 
 public record Burrow(List<Amphipod> Amphipods, int SideRoomDepth)
 {
+    private static readonly int[] HallwayX = { 1, 2, 4, 6, 8, 10, 11 };
+
     public bool IsFinished() => Amphipods.All(a => a.IsHome());
 
     public int ComputePriority() => Amphipods.Count(a => !a.Exhausted);
@@ -12,12 +14,71 @@ public record Burrow(List<Amphipod> Amphipods, int SideRoomDepth)
 
     public bool TryFindMoveToSideRoom(out Burrow burrow, out int cost)
     {
-        throw new NotImplementedException("TODO");
+        var idx = -1;
+        foreach (var amphipod in Amphipods)
+        {
+            idx += 1;
+            if (amphipod.Exhausted || amphipod.IsHome() || !CanLeaveSideRoom(amphipod))
+                continue;
+
+            if (TryFindPlaceInSideRoom(amphipod, out var node))
+            {
+                if (PathThroughHallwayIsFree(amphipod, node))
+                {
+                    (burrow, cost) = WithUpdatedAmphipod(idx, amphipod, node);
+                    return true;
+                }
+            }
+        }
+
+        burrow = null!;
+        cost = 0;
+        return false;
     }
 
     public IEnumerable<(Burrow Next, int Cost)> FindMovesToHallway()
     {
+        var moves = new List<(Burrow, int)>();
+        var idx = -1;
+        foreach (var amphipod in Amphipods)
+        {
+            idx += 1;
+            if (amphipod.Exhausted || !amphipod.IsInSideRoom() || !CanLeaveSideRoom(amphipod))
+                continue;
+
+            var neighbours = HallwayX
+                .Select(x => new Node(1, x))
+                .Where(node => PathThroughHallwayIsFree(amphipod, node))
+                .Where(node => !CreatesDeadlock(amphipod, node));
+
+            moves.AddRange(neighbours.Select(neighbour => WithUpdatedAmphipod(idx, amphipod, neighbour)));
+        }
+
+        return moves;
+    }
+
+    private bool CanLeaveSideRoom(Amphipod amphipod)
+    {
         throw new NotImplementedException("TODO");
     }
 
+    private bool TryFindPlaceInSideRoom(Amphipod amphipod, out Node node)
+    {
+        throw new NotImplementedException("TODO");
+    }
+
+    private bool PathThroughHallwayIsFree(Amphipod amphipod, Node to)
+    {
+        throw new NotImplementedException("TODO");
+    }
+
+    private (Burrow, int) WithUpdatedAmphipod(int idx, Amphipod amphipod, Node node)
+    {
+        throw new NotImplementedException("TODO");
+    }
+
+    private bool CreatesDeadlock(Amphipod amphipod, Node to)
+    {
+        throw new NotImplementedException("TODO");
+    }
 }
