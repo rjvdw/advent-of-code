aea62ae06a8cf223d973d16d25091b33a76ce7a9
Ruud van der Weide <rj@rdcl.dev>
2023-12-05 23:39:31 +0100

    Greatly improved the performance for the rust solution for 2023, day 5

--- rust-v2/aoc2023/src/day05/main.rs
+++ rust-v2/aoc2023/src/day05/main.rs
@@ -9,9 +9,12 @@ use rdcl_aoc_core::input::InputReader;
 use rdcl_aoc_core::parser::parse_whitespace_separated_to_vec;
 use rdcl_aoc_core::MainResult;
 
-use crate::mapping::{Mapping, Mappings};
+use crate::mapping::{Mappable, Mapping, Mappings};
 
 mod mapping;
+
+type Seeds = Vec<u64>;
+type SeedRanges = Vec<(u64, u64)>;
 
 #[derive(Parser, Debug)]
 #[clap(about = "The solution for advent of code 2023, day 5")]
@@ -27,14 +30,14 @@ fn main() -> MainResult {
     let (naive_seeds, seed_ranges) = parse_seeds(&mut input)?;
     let mappings = <Vec<Mapping>>::parse(&mut input)?;
 
-    let locations = evaluate_mappings(&naive_seeds, &mappings);
+    let locations = evaluate_seeds(&naive_seeds, &mappings);
     match locations.iter().min() {
         Some(v) => println!("The lowest location number is {}", v),
         None => println!("No location numbers were found"),
     };
 
-    let locations = evaluate_mappings(&seed_ranges, &mappings);
-    match locations.iter().min() {
+    let locations = evaluate_seed_ranges(&seed_ranges, &mappings);
+    match find_min(&locations) {
         Some(v) => println!("Using the full ranges, the lowest location number is {}", v),
         None => println!("Using the full ranges, no location numbers were found"),
     };
@@ -42,7 +45,7 @@ fn main() -> MainResult {
     Ok(())
 }
 
-fn parse_seeds<T>(input: &mut T) -> Result<(Vec<u64>, Vec<u64>), ParseError>
+fn parse_seeds<T>(input: &mut T) -> Result<(Seeds, SeedRanges), ParseError>
 where
     T: Iterator<Item = String>,
 {
@@ -56,20 +59,26 @@ where
     let mut iter = naive_seeds.iter();
     while let Some(&start) = iter.next() {
         let end = start + *iter.next().ok_or(())?;
-        for i in start..end {
-            seed_ranges.push(i);
-        }
+        seed_ranges.push((start, end));
     }
 
     Ok((naive_seeds, seed_ranges))
 }
 
-fn evaluate_mappings(seeds: &[u64], mappings: &Vec<Mapping>) -> Vec<u64> {
-    let mut locations: Vec<u64> = Vec::with_capacity(seeds.len());
+fn evaluate_seeds(seeds: &[u64], mappings: &Vec<Mapping>) -> Seeds {
+    let mut locations: Seeds = Vec::with_capacity(seeds.len());
     for seed in seeds {
-        locations.push(mappings.apply(*seed));
+        locations.push(mappings.apply_to_nr(*seed));
     }
     locations
+}
+
+fn evaluate_seed_ranges(seed_ranges: &[(u64, u64)], mappings: &Vec<Mapping>) -> SeedRanges {
+    mappings.apply_to_ranges(seed_ranges)
+}
+
+fn find_min(ranges: &[(u64, u64)]) -> Option<u64> {
+    ranges.iter().map(|(start, _)| start).copied().min()
 }
 
 #[cfg(test)]
@@ -85,7 +94,7 @@ mod tests {
         let mut input = test_data().peekable();
         let (seeds, _) = parse_seeds(&mut input).unwrap();
         let mappings = <Vec<Mapping>>::parse(&mut input).unwrap();
-        let locations = evaluate_mappings(&seeds, &mappings);
+        let locations = evaluate_seeds(&seeds, &mappings);
 
         assert_eq!(locations, [82, 43, 86, 35]);
     }
@@ -95,13 +104,19 @@ mod tests {
         let mut input = test_data().peekable();
         let (_, seeds) = parse_seeds(&mut input).unwrap();
         let mappings = <Vec<Mapping>>::parse(&mut input).unwrap();
-        let locations = evaluate_mappings(&seeds, &mappings);
+        let mut locations = evaluate_seed_ranges(&seeds, &mappings);
+        locations.sort_by_key(|l| l.0);
 
         assert_eq!(
             locations,
             [
-                82, 83, 84, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 60, 86, 87, 88, 89, 94, 95, 96,
-                56, 57, 58, 59, 97, 98
+                (46, 56),
+                (56, 60),
+                (60, 61),
+                (82, 85),
+                (86, 90),
+                (94, 97),
+                (97, 99)
             ]
         );
     }
--- rust-v2/aoc2023/src/day05/mapping.rs
+++ rust-v2/aoc2023/src/day05/mapping.rs
@@ -4,11 +4,34 @@ use std::str::FromStr;
 
 use rdcl_aoc_core::error::ParseError;
 
+pub trait Mappable {
+    fn apply_to_nr(&self, nr: u64) -> u64;
+
+    fn apply_to_ranges(&self, ranges: &[(u64, u64)]) -> Vec<(u64, u64)>;
+}
+
 pub trait Mappings {
     fn parse<T>(input: &mut Peekable<T>) -> Result<Vec<Mapping>, ParseError>
     where
         T: Iterator<Item = String>;
-    fn apply(&self, nr: u64) -> u64;
+}
+
+impl Mappable for Vec<Mapping> {
+    fn apply_to_nr(&self, nr: u64) -> u64 {
+        let mut result = nr;
+        for mapping in self {
+            result = mapping.apply_to_nr(result);
+        }
+        result
+    }
+
+    fn apply_to_ranges(&self, ranges: &[(u64, u64)]) -> Vec<(u64, u64)> {
+        let mut result = ranges.to_vec();
+        for mapping in self {
+            result = mapping.apply_to_ranges(&result);
+        }
+        result
+    }
 }
 
 impl Mappings for Vec<Mapping> {
@@ -21,14 +44,6 @@ impl Mappings for Vec<Mapping> {
             mappings.push(Mapping::parse(&mut input)?);
         }
         Ok(mappings)
-    }
-
-    fn apply(&self, nr: u64) -> u64 {
-        let mut result = nr;
-        for mapping in self {
-            result = mapping.apply(result);
-        }
-        result
     }
 }
 
@@ -49,28 +64,62 @@ impl Mapping {
             if line.is_empty() {
                 break;
             }
-
             mapping.ranges.push(line.parse()?);
         }
 
         Ok(mapping)
     }
+}
 
-    pub fn apply(&self, nr: u64) -> u64 {
+impl Mappable for Mapping {
+    fn apply_to_nr(&self, nr: u64) -> u64 {
         for range in &self.ranges {
             if let Some(v) = range.apply(nr) {
                 return v;
             }
         }
-
         nr
     }
+
+    fn apply_to_ranges(&self, source_range: &[(u64, u64)]) -> Vec<(u64, u64)> {
+        let mut unmatched = source_range.to_vec();
+        let mut matched = vec![];
+        for mapping_range in &self.ranges {
+            let mut next_unmatched = vec![];
+            for range in unmatched {
+                let result = mapping_range.apply_to_range(range);
+
+                if let Some(v) = result.unmatched_left {
+                    next_unmatched.push(v);
+                }
+                if let Some(v) = result.unmatched_right {
+                    next_unmatched.push(v);
+                }
+                if let Some(v) = result.matched {
+                    matched.push(v);
+                }
+            }
+            unmatched = next_unmatched;
+        }
+        for range in unmatched {
+            matched.push(range);
+        }
+        matched
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct ApplyToRangeResult {
+    unmatched_left: Option<(u64, u64)>,
+    unmatched_right: Option<(u64, u64)>,
+    matched: Option<(u64, u64)>,
 }
 
 #[derive(Debug, Clone)]
 struct MappingRange {
     destination_range_start: u64,
     source_range_start: u64,
+    range_length: u64,
     source_range: Range<u64>,
 }
 
@@ -83,6 +132,55 @@ impl MappingRange {
             None
         }
     }
+
+    fn apply_to_range(&self, (start, end): (u64, u64)) -> ApplyToRangeResult {
+        let mut result = ApplyToRangeResult {
+            unmatched_left: None,
+            unmatched_right: None,
+            matched: None,
+        };
+
+        let source_range_start = self.source_range_start;
+        let source_range_end = source_range_start + self.range_length;
+
+        if end < source_range_start {
+            // no overlap at all
+            result.unmatched_left = Some((start, end));
+        } else if start >= source_range_end {
+            // no overlap at all
+            result.unmatched_right = Some((start, end));
+        } else if start >= source_range_start && end <= source_range_end {
+            // full overlap
+            let offset = start - source_range_start;
+            let size = end - start;
+            let start = self.destination_range_start + offset;
+            let end = start + size;
+            result.matched = Some((start, end));
+        } else if start < source_range_start && end > source_range_end {
+            // full overlap
+            result.unmatched_left = Some((start, source_range_start));
+            result.unmatched_right = Some((source_range_end, end));
+            result.matched = Some((
+                self.destination_range_start,
+                self.destination_range_start + self.range_length,
+            ));
+        } else if start < source_range_start {
+            // partial overlap at the start
+            result.unmatched_left = Some((start, source_range_start));
+            let size = end - source_range_start;
+            let start = self.destination_range_start;
+            let end = start + size;
+            result.matched = Some((start, end));
+        } else {
+            // partial overlap at the end
+            result.unmatched_right = Some((source_range_end, end));
+            let start = self.destination_range_start + start - source_range_start;
+            let end = self.destination_range_start + self.range_length;
+            result.matched = Some((start, end));
+        }
+
+        result
+    }
 }
 
 impl FromStr for MappingRange {
@@ -94,11 +192,12 @@ impl FromStr for MappingRange {
 
         let destination_range_start = s[0..pos1].parse()?;
         let source_range_start = s[pos1 + 1..pos2].parse()?;
-        let range_length = s[pos2 + 1..].parse::<u64>()?;
+        let range_length = s[pos2 + 1..].parse()?;
 
         Ok(MappingRange {
             destination_range_start,
             source_range_start,
+            range_length,
             source_range: source_range_start..source_range_start + range_length,
         })
     }
@@ -119,13 +218,13 @@ mod tests {
 
         let mapping = Mapping::parse(&mut input).unwrap();
 
-        assert_eq!(mapping.apply(79), 81);
-        assert_eq!(mapping.apply(14), 14);
-        assert_eq!(mapping.apply(55), 57);
-        assert_eq!(mapping.apply(13), 13);
-        assert_eq!(mapping.apply(98), 50);
-        assert_eq!(mapping.apply(99), 51);
-        assert_eq!(mapping.apply(100), 100);
+        assert_eq!(mapping.apply_to_nr(79), 81);
+        assert_eq!(mapping.apply_to_nr(14), 14);
+        assert_eq!(mapping.apply_to_nr(55), 57);
+        assert_eq!(mapping.apply_to_nr(13), 13);
+        assert_eq!(mapping.apply_to_nr(98), 50);
+        assert_eq!(mapping.apply_to_nr(99), 51);
+        assert_eq!(mapping.apply_to_nr(100), 100);
     }
 
     #[test]
@@ -145,13 +244,13 @@ mod tests {
 
         let mappings = <Vec<Mapping>>::parse(&mut input).unwrap();
 
-        assert_eq!(mappings.apply(79), 81);
-        assert_eq!(mappings.apply(14), 53);
-        assert_eq!(mappings.apply(55), 57);
-        assert_eq!(mappings.apply(13), 52);
-        assert_eq!(mappings.apply(98), 35);
-        assert_eq!(mappings.apply(99), 36);
-        assert_eq!(mappings.apply(100), 100);
+        assert_eq!(mappings.apply_to_nr(79), 81);
+        assert_eq!(mappings.apply_to_nr(14), 53);
+        assert_eq!(mappings.apply_to_nr(55), 57);
+        assert_eq!(mappings.apply_to_nr(13), 52);
+        assert_eq!(mappings.apply_to_nr(98), 35);
+        assert_eq!(mappings.apply_to_nr(99), 36);
+        assert_eq!(mappings.apply_to_nr(100), 100);
     }
 
     #[test]
@@ -162,5 +261,84 @@ mod tests {
         assert_eq!(range.apply(14), None);
         assert_eq!(range.apply(55), Some(57));
         assert_eq!(range.apply(13), None);
+    }
+
+    #[test]
+    fn test_mapping_range_apply_to_range() {
+        let range = "50 20 10".parse::<MappingRange>().unwrap();
+
+        assert_eq!(
+            range.apply_to_range((0, 10)),
+            ApplyToRangeResult {
+                unmatched_left: Some((0, 10)),
+                unmatched_right: None,
+                matched: None
+            }
+        );
+        assert_eq!(
+            range.apply_to_range((15, 25)),
+            ApplyToRangeResult {
+                unmatched_left: Some((15, 20)),
+                unmatched_right: None,
+                matched: Some((50, 55))
+            }
+        );
+        assert_eq!(
+            range.apply_to_range((20, 30)),
+            ApplyToRangeResult {
+                unmatched_left: None,
+                unmatched_right: None,
+                matched: Some((50, 60))
+            }
+        );
+        assert_eq!(
+            range.apply_to_range((15, 35)),
+            ApplyToRangeResult {
+                unmatched_left: Some((15, 20)),
+                unmatched_right: Some((30, 35)),
+                matched: Some((50, 60))
+            }
+        );
+        assert_eq!(
+            range.apply_to_range((25, 35)),
+            ApplyToRangeResult {
+                unmatched_left: None,
+                unmatched_right: Some((30, 35)),
+                matched: Some((55, 60))
+            }
+        );
+    }
+
+    #[test]
+    fn test_multiple_mapping_ranges_applied_to_range() {
+        let mut input = "test map:
+            45 77 23
+            81 45 19
+            68 64 13
+            "
+        .lines()
+        .map(|line| line.trim().to_string())
+        .peekable();
+
+        let mappings = <Vec<Mapping>>::parse(&mut input).unwrap();
+        let ranges = vec![(74, 88), (54, 63), (46, 50)];
+
+        // 74..88
+        //   - matches with 45 77 23 and 68 64 13:
+        //     - split into 74..77 and 77..88
+        //     - map 74..77 to 78..81
+        //     - map 77..88 to 45..56
+        //     - end up with 74..77 and 45..56
+        // 54..64
+        //   - matches with 81 45 19:
+        //     - map 54..63 to 90..99
+        // 46..50
+        //   - matches with 81 45 19:
+        //     - map 46..50 to 82..86
+
+        let mut actual = mappings.apply_to_ranges(&ranges);
+        actual.sort_by_key(|v| v.0);
+
+        assert_eq!(actual, vec![(45, 56), (78, 81), (82, 86), (90, 99)]);
     }
 }
